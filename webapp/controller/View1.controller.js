2998bd0d43c2aa26aa676e22c3c30ac5f8c18fbb
Sneha Soyuz <sneha@soyuztechnologies.com>
2026-04-14 22:06:34 +0530

    Language and filter

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,10 +1,32 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+
+], (Controller, Filter, FilterOperator) => {
     "use strict";
 
     return Controller.extend("com.excnorthwindproject.excnorthwindproject.controller.View1", {
         onInit() {
+        },
+        onSearch: function (oEvent) {
+            var sVal = oEvent.getParameter("newValue");
+            var oFilter = new Filter("ProductName", FilterOperator.Contains, sVal);
+            this.getView().byId("idTable").getBinding("items").filter(oFilter)
+        },
+        onChangeLanguage: function (oEvent) {
+            var sLang = oEvent.getSource().getSelectedKey(); // "en" or "es"
+
+            // Change language globally
+            sap.ui.getCore().getConfiguration().setLanguage(sLang);
+
+            // Refresh model (important)
+            var oModel = new sap.ui.model.resource.ResourceModel({
+                bundleName: "com.excnorthwindproject.excnorthwindproject.i18n.i18n",
+                bundleLocale: sLang
+            });
+
+            this.getView().setModel(oModel, "i18n");
         }
     });
 });
